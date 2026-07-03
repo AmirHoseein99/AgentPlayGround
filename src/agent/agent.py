d87f472ca09a9cc99e64d154a9509c9d0352dc02
AmirHoseein99 <amirimani491@gmail.com>
2026-07-03 23:24:45 +0330

    refactor: :recycle: Update agent and API to use new Agent class structure and improve tool registration

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -1,8 +1,10 @@
+from asyncio.log import logger
+
 from ..llm.prompt import AGENT_SYSTEM_PROMPT
 from ..llm.openrouter import OpenRouterAPI
 from .parser import agent_format_response
-from .tools.web_search import web_search_tool
-from .tools.python_executor import python_executor_tool
+from .tools.web_search import WebSearchTool
+from .tools.python_executor import PythonExecutorTool
 from ..logger import get_logger
 from ..memory.memory_manager import (
     initialize_conversation,
@@ -10,113 +12,158 @@ from ..memory.memory_manager import (
     append_to_conversation,
 )
 import json
-
-tool_calling = {"web_search": web_search_tool, "python_executor": python_executor_tool}
+from .tools.base import BaseTool
 
 max_steps = 10
 
 
-def call_agent(user_input, conversation_id: str):
+class Agent:
+    def __init__(self):
+        self.logger = get_logger("agent")
+        self.llm_api = OpenRouterAPI()
+        self.tools: dict[str, BaseTool] = {}
 
-    initialize_conversation(conversation_id=conversation_id)
-    append_to_conversation(
-        role="user", content=user_input, conversation_id=conversation_id
-    )
+    def register_tool(self, tool: BaseTool):
+        self.tools[tool.name] = tool
 
-    logger = get_logger("agent")
+    def get_registered_tools(self):
+        return list(self.tools.keys())
 
-    messages = [
-        {"role": "system", "content": AGENT_SYSTEM_PROMPT},
-        *get_context(conversation_id),
-    ]
-    llm_api = OpenRouterAPI()
+    def get_tool(self, tool_name: str):
+        if tool_name not in self.tools:
+            self.logger.error(f"Tool '{tool_name}' not found.")
+            return None
+        return self.tools.get(tool_name)
 
-    for i in range(max_steps):
-        logger.info(f"Step {i + 1}/{max_steps}: Sending messages to OpenRouter API.")
+    def tool_description(self, tool_name: str):
+        tool = self.get_tool(tool_name)
+        if tool:
+            return tool.description
+        else:
+            return f"Tool '{tool_name}' not found."
 
-        logger.info("Calling OpenRouter API...")
-        llm_response = llm_api.call_openrouter_api(messages=messages)
-        logger.info(f"llm response in agent, {llm_response}")
+    @property
+    def tool_definitions(self):
+        return [
+            {
+                "name": tool.name,
+                "description": tool.description,
+                "schema": tool.schema,
+            }
+            for tool in self.tools.values()
+        ]
 
-        try:
-            parsed_response = agent_format_response(llm_response)
-            append_to_conversation(
-                role="assistant",
-                content=parsed_response,
-                conversation_id=conversation_id,
+    def run(self, user_input, conversation_id: str):
+        initialize_conversation(conversation_id=conversation_id)
+        append_to_conversation(
+            role="user", content=user_input, conversation_id=conversation_id
+        )
+        messages = [
+            {"role": "system", "content": AGENT_SYSTEM_PROMPT},
+            *get_context(conversation_id),
+        ]
+        for i in range(max_steps):
+            self.logger.info(
+                f"Step {i + 1}/{max_steps}: Sending messages to OpenRouter API."
             )
-            messages.append(
-                {"role": "assistant", "content": json.dumps(parsed_response)}
-            )
-        except Exception as e:
-            logger.exception(e)
-            messages.append(
-                {
-                    "role": "user",
-                    "content": f"""
-                        Your previous response could not be parsed.
 
-                        Error:
-                        {e}
-
-                        Please produce a valid structured response.
-                        """,
-                }
-            )
-            continue
-
-        logger.info(f"Parsed llm response: {parsed_response}")
-
-        # --------------------
-        # FINAL RESPONSE
-        # --------------------
-        if parsed_response.get("type") == "final":
-            logger.info(f"Agent Final Response : {parsed_response}")
-            return parsed_response["message"]
-
-        # --------------------
-        # TOOL CALL
-        # --------------------
-        if parsed_response.get("type") == "tool_call":
-            tool_name = parsed_response.get("tool")
-            tool_args = parsed_response.get("args")
-
-            tool = tool_calling.get(tool_name)
-
-            if tool is None:
-                logger.error(f"Tool not found: {tool_name}")
-                continue
+            self.logger.info("Calling OpenRouter API...")
+            llm_response = self.llm_api.call_openrouter_api(messages=messages)
+            self.logger.info(f"llm response in agent, {llm_response}")
             try:
-                if not isinstance(tool_args, dict):
-                    raise ValueError(f"tool_args must be dict, got {type(tool_args)}")
-
-                tool_result = tool(**tool_args)
+                parsed_response = agent_format_response(llm_response)
                 append_to_conversation(
-                    role="tool",
-                    content=tool_result,
+                    role="assistant",
+                    content=parsed_response,
                     conversation_id=conversation_id,
-                    tool_name=tool_name,
                 )
                 messages.append(
+                    {"role": "assistant", "content": json.dumps(parsed_response)}
+                )
+            except Exception as e:
+                self.logger.exception(e)
+                messages.append(
                     {
-                        "role": "tool",
-                        "content": json.dumps(
-                            {"tool": tool_name, "result": tool_result}
-                        ),
+                        "role": "user",
+                        "content": f"""
+                            Your previous response could not be parsed.
+
+                            Error:
+                            {e}
+
+                            Please produce a valid structured response.
+                            """,
                     }
                 )
                 continue
-            except Exception as e:
-                logger.exception(e)
-                append_to_conversation(
-                    role="tool",
-                    content=f"Tool execution failed: {e}",
-                    conversation_id=conversation_id,
-                    tool_name=tool_name,
-                )
-                messages.append(
-                    {"role": "tool", "content": f"Tool execution failed: {e}"}
-                )
+            self.logger.info(f"Parsed llm response: {parsed_response}")
+            # --------------------
+            # FINAL RESPONSE
+            # --------------------
+            if parsed_response.get("type") == "final":
+                self.logger.info(f"Agent Final Response : {parsed_response}")
+                return parsed_response["message"]
+            # --------------------
+            # TOOL CALL
+            # --------------------
+            if parsed_response.get("type") == "tool_call":
+                tool_name = parsed_response.get("tool")
+                tool_args = parsed_response.get("args")
 
-    logger.warning("Sorry, I couldn't complete the task within the step limit.")
-    return
+                tool = self.get_tool(str(tool_name))
+
+                if tool is None:
+                    self.logger.error(f"Tool not found: {tool_name}")
+                    messages.append(
+                        {
+                            "role": "tool",
+                            "content": json.dumps(
+                                {
+                                    "tool": tool_name,
+                                    "result": f"tool {tool_name} not found",
+                                }
+                            ),
+                        }
+                    )
+                try:
+                    # TODO : add a validation for tool's args
+                    if not isinstance(tool_args, dict):
+                        raise ValueError(
+                            f"tool_args must be dict, got {type(tool_args)}"
+                        )
+
+                    tool_result = tool.run(**tool_args)
+                    append_to_conversation(
+                        role="tool",
+                        content=tool_result,
+                        conversation_id=conversation_id,
+                        tool_name=tool_name,
+                    )
+                    messages.append(
+                        {
+                            "role": "tool",
+                            "content": json.dumps(
+                                {"tool": tool_name, "result": tool_result}
+                            ),
+                        }
+                    )
+                    continue
+                except Exception as e:
+                    self.logger.exception(e)
+                    append_to_conversation(
+                        role="tool",
+                        content=f"Tool execution failed: {e}",
+                        conversation_id=conversation_id,
+                        tool_name=tool_name,
+                    )
+                    messages.append(
+                        {"role": "tool", "content": f"Tool execution failed: {e}"}
+                    )
+        logger.warning("Sorry, I couldn't complete the task within the step limit.")
+        return
+
+
+agent = Agent()
+
+agent.register_tool(PythonExecutorTool())
+agent.register_tool(WebSearchTool())
--- src/agent/api.py
+++ src/agent/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from .agent import call_agent
+from .agent import agent
 from ..logger import get_logger
 
 agent_router = APIRouter(prefix="/agent", tags=["agent"])
@@ -11,7 +11,7 @@ async def call_agent_endpoint(user_input: str, conversation_id: str = None):
     logger.info(f"Received user input: {user_input}, {conversation_id}")
 
     try:
-        response = call_agent(user_input, conversation_id)
+        response = agent.run(user_input, conversation_id)
         logger.info(f"Agent response: {response}")
         return {"response": response}
     except Exception as e:
