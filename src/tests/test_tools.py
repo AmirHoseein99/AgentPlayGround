03fb9a261e2cc2242512c4dc2444b6d09e94d084
AmirHoseein99 <amirimani491@gmail.com>
2026-07-11 17:52:29 +0330

    refactor: :recycle: Improve test_web_search_tool by adding mock for TavilyClient and enhancing readability

--- src/tests/test_tools.py
+++ src/tests/test_tools.py
@@ -1,11 +1,26 @@
 from src.agent.tools.web_search import WebSearchTool
 from src.agent.tools.python_executor import PythonExecutorTool
+from src.agent.tools.web_search import WebSearchTool
+from unittest.mock import Mock, patch
 
 
-def test_web_search_tool():
+
+
+
+@patch("src.agent.tools.web_search.TavilyClient")
+def test_web_search_tool(mock_client):
+    mock_instance = Mock()
+    mock_instance.search.return_value = {
+        "results": [
+            {"title": "Hello", "content": "World"}
+        ]
+    }
+
+    mock_client.return_value = mock_instance
+
     tool = WebSearchTool()
-
     result = tool.execute(query="hello")
+
     assert result is not None
 
 
